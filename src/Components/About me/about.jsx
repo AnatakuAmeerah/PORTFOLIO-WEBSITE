4a8eae74c14ddde77f6f7c4ae5ebd2f924ca5410
anataku ameerah <anatakuameerah@gmail.com>
2025-12-30 20:41:18 +0100

    image change

--- src/Components/About me/about.jsx
+++ src/Components/About me/about.jsx
@@ -6,7 +6,7 @@ function About() {
             <section className="about-me-bg">
                 <h2 className='project-head' style={{ color: 'rgba(240, 127, 240, 0.801)', paddingTop: '0' }}>ABOUT ME</h2>
                 <div className='pic-text-cont'>
-                    <img className='mirah-pic' src="Ameerah.svg" alt="profile-pic" />
+                    <img className='mirah-pic' src="IMG_3646-Edit (1).webp" alt="profile-pic" />
                     <p className="about-p">
                         Ameerah is a creative and highly motivated frontend developer with a diploma in Frontend Engineering from AltSchoolAfrica, she is proficient in web development using frameworks and languages like
                         HTML, CSS, JavaScript, React, Vue and Typescript. She is also well versed in the implementation of backend technologies and able to implement web design concepts that are scalable and user friendly.
